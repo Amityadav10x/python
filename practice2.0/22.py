2cdf81d3e9f73d52ef3e587962db126828c99f94
Amityadav10x <ay3040869@gmail.com>
2024-03-18 10:54:38 +0530

    new commit

--- practice2.0/22.py
+++ practice2.0/22.py
@@ -4,9 +4,6 @@ def extract_even(lst):
     return [x for x in lst if x%2==0]
 print(extract_even([10,11,12,11,13]))
 
-# extract_odd numbers 
 
-def extract_odd(lst):
-    return [x for x in lst if x%2!=0]
-print(extract_odd([10,11,12,13]))
+# to accept 10 element in list and count total odd element that index number is also odd
 
---
+++ practice2.0/practice3.o/list.py
@@ -0,0 +1,61 @@
+# l = []
+# print(type(l))
+
+# t = 5
+# print(type(t,))
+# print(type(t))
+
+
+# a = list(range(10))
+# print(a)
+
+# s  = "united"
+# a =  list(s)
+# print(a)
+
+
+# s = "abcdef"
+# l = s.split('c')
+# print(l)
+
+
+# l = [5,6,7,'a','b','c']
+# l.append(10)
+# print(l)
+
+# write a programm to insert ten element in the list
+
+# l = []
+
+# for i in range(0,10,1):
+#     x = int(input("enter a number :"))
+#     l.append(x)
+# print(i)
+    
+
+# l = [5,10,12,12,141]
+
+# for i in range (len(l)):
+#     print(l(i))
+
+# to accept 10 element in list and print all even element
+
+
+
+l = [5,10,12,12,141]
+
+for i in l:
+    if i%2==0:
+        print(i)
+
+
+# to accept 10 element in list and count total odd element that index number is also odd
+
+
+
+l = [5,10,12,12,141]
+
+for i in l:
+    if i%2 != 0:
+        print(i)
+    
