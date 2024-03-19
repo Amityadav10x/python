f29563ae53832fb9cad75e4ec96b70f1ec3b8f76
Amityadav10x <ay3040869@gmail.com>
2024-03-19 18:43:24 +0530

    new commit

---
+++ practice2.0/practice3.o/23.py
@@ -0,0 +1,10 @@
+num = int(input("enter values to perform operations :"))
+
+for i in range (0,5):
+    if num == 0:
+        print("yes")
+    elif num == 12:
+        print("no")
+    else:
+        print("neither 0 nor 12")
+        
